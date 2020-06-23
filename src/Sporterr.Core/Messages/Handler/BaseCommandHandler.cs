394a3b24be2ed642efd2eaf4de130a02b196bb71
Andrei Franz Salvador <andrei.salvador@lincros.com>
2020-06-22 22:11:56 -0300

    TO ENGLISH

--- src/Sporterr.Core/Messages/Handler/BaseCommandHandler.cs
+++ src/Sporterr.Core/Messages/Handler/BaseCommandHandler.cs
@@ -21,19 +21,19 @@ namespace Sporterr.Core.Messages.Handler
             _mediatr = mediatr;
         }
 
-        protected async Task<bool> Salvar() => await _repository.Commit();
+        protected async Task<bool> Save() => await _repository.Commit();
 
-        protected async Task<bool> NotifyAndReturn(string mensagem)
+        protected async Task<bool> NotifyAndReturn(string message)
         {
-            await _mediatr.Notify(new DomainNotification(typeof(TAggregateRoot).Name, mensagem));
+            await _mediatr.Notify(new DomainNotification(typeof(TAggregateRoot).Name, message));
             return false;
         }
 
-        protected async Task<bool> SaveAndPublish<TEvent>(TEvent evento) where TEvent : Event
+        protected async Task<bool> SaveAndPublish<TEvent>(TEvent @event) where TEvent : Event
         {
-            bool salvou = await Salvar();
+            bool salvou = await Save();
 
-            if (salvou) await _mediatr.Publish<TEvent>(evento);
+            if (salvou) await _mediatr.Publish<TEvent>(@event);
 
             return salvou;
         }
