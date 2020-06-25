49aa732e99945a81bdecbaaf9b7c9819c8c37421
Andrei Franz Salvador <andrei.salvador@lincros.com>
2020-06-25 10:53:52 -0300

    Ajuste

--- src/Sporterr.Cadastro.Application/Events/Handlers/EmpresaEventHandler.cs
+++ src/Sporterr.Cadastro.Application/Events/Handlers/EmpresaEventHandler.cs
@@ -1,12 +1,8 @@
 ﻿using MediatR;
 using Sporterr.Cadastro.Application.Commands;
 using Sporterr.Cadastro.Data.Repository.Interfaces;
-using Sporterr.Cadastro.Domain;
 using Sporterr.Core.Communication.Mediator;
 using Sporterr.Core.Messages.CommonMessages.IntegrationEvents.Solicitacoes;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
